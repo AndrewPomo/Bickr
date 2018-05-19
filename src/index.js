eb16677fc26df3dbd656b205c8879353877f5d13
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-05-19 12:17:27 -0700

    initial skeleton

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Signup from './signup'
+class App extends React.Component {
+render() {
+    return (
+      <div>
+        Welcome to Definder! Sign up to get started
+      </div>
+    );
+  }
+}
+render(<App />, document.getElementById('app'));
---
+++ src/signup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+const Signup = (props) => {
+  return (
+    <div>
+      Here's a signup
+    </div>
+  );
+}
+export default Signup;
